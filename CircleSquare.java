37fafe8cc635235c20ee6f624426beb00111b74f
lyyacmlyt <93073756+lyyacmlyt@users.noreply.github.com>
2021-10-25 23:49:03 +0800

    Add files via upload

---
+++ CircleSquare.java
@@ -0,0 +1,9 @@
+public class CircleSquare {
+    public static void main(String[] args){
+        int r=3;
+        double pi =3.14;
+        double S=pi*r*r;
+        double C=2*pi*r;
+        System.out.printf("C=:%f;S=:%f",C,S);
+    }
+};
