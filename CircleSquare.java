81b1f95e096e560163e02af63383bda774e05182
lyyacmlyt <93073756+lyyacmlyt@users.noreply.github.com>
2021-11-07 23:59:09 +0800

    Delete CircleSquare.java

--- CircleSquare.java
+++
@@ -1,9 +0,0 @@
-public class CircleSquare {
-    public static void main(String[] args){
-        int r=3;
-        double pi =3.14;
-        double S=pi*r*r;
-        double C=2*pi*r;
-        System.out.printf("C=:%f;S=:%f",C,S);
-    }
-};
